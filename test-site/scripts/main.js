213a70091fcaafead2e36c90d117ee508740cff2
PerfumeAir <156283088+PerfumeAir@users.noreply.github.com>
2024-01-11 17:46:11 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,26 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("请输入你的名字。");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Hello," + myName;
+
+  }
+}
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Hello," + storedName;
+
+  }
+  
+myButton.onclick = function () {
+    setUserName();
+  };
+  
